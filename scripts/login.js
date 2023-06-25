01f7cdfa82b34f9d4ecd7c04ea92e9cfccc54555
WhiteLord <57297036+jorik004@users.noreply.github.com>
2023-06-25 21:49:40 +0600

    Update login.js

--- scripts/login.js
+++ scripts/login.js
@@ -17,7 +17,7 @@ async function main() {
         return data
     }
     async function getAdmin(login, password) {
-        const res = await fetch('http://localhost:3000/adminpanel', {
+        const res = await fetch('http://31.129.96.64:3000/adminpanel', {
             method: 'post',
             headers: {
                 'Content-Type': 'application/json'
